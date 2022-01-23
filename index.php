9ad2eab0b9da96a898d226e38dcf758b538eae5a
Winton Almundarisna <wintonalmundarisna@gmail.com>
2022-01-23 22:41:57 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link rel="stylesheet" href="../CSS/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+  <title>PT. Citra Sentosa Solusindo</title>
+  <link rel="icon" href="IMG/ico.png">
+</head>
+
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-brand" href="#"><img class="logoNav" src="../IMG/logo_fix2.png" alt="" /> <span> Citra Sentosa Solusindo </span></div>
+      <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="menu" aria-current="page" href="#navbar">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="garis" aria-current="page">|</a>
+          </li>
+          <li class="nav-item">
+            <a class="menu" aria-current="page" href="#product">PRODUCT</a>
+          </li>
+          <li class="nav-item">
+            <a class="garis" aria-current="page">|</a>
+          </li>
+          <li class="nav-item">
+            <a class="menu" aria-current="page" href="#about">ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="garis" aria-current="page">|</a>
+          </li>
+          <li class="nav-item">
+            <a class="menu" aria-current="page" href="#client">CLIENT</a>
+          </li>
+          <li class="nav-item">
+            <a class="garis" aria-current="page">|</a>
+          </li>
+          <li class="nav-item">
+            <a class="menu" aria-current="page" href="#message">MESSAGE</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Navbar -->
+  <!-- Content 1 -->
+  <div class="content-one" id="navbar">
+    <!-- Carousel -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="car-satu carousel-item active">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>THE FUTURE TECHNOLOGY</h5>
+            <p>Let's make life better with us</p>
+          </div>
+        </div>
+        <div class="car-dua carousel-item">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>BEST QUALITY PRODUCT</h5>
+            <p>We have standard international product for you</p>
+          </div>
+        </div>
+        <div class="car-tiga carousel-item">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>LOW BUDGET</h5>
+            <p>Don't worry about it, just come and find what you want</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    <!-- Carousel -->
+  </div>
+  <!-- Content 1 -->
+  <!-- Content 2 -->
+  <div class="konten-dua" id="product">
+    <div class="satu">
+      <div class="gambar1"></div>
+      <div class="tulisan1">
+        <p class="h1">Infrastructure</p>
+        <p class="p1">Want to have a beautiful infrastructure but confused about brand and price, don't worry, we have everything you need with friendly price, let's exceed expectations and together achieve dreams.</p>
+        <button onClick="pop()">KNOW MORE</button>
+      </div>
+    </div>
+    <div class="dua">
+      <div class="gambar2"></div>
+      <div class="tulisan2">
+        <p class="h2">IT Consultant</p>
+        <p class="p2">Come and ask us, don't worry about price and quality, we are a professional team with international standard industries, we will give you best answer for every problem you have.</p>
+        <button onClick="pop()">KNOW MORE</button>
+      </div>
+    </div>
+    <div class="tiga">
+      <div class="gambar3"></div>
+      <div class="tulisan3">
+        <p class="h3">Branches Solution</p>
+        <p class="p3">let's negotiate with us to removing many problem issues you have and of course all as you want, we'll help you make that happen, and don't worry about price.</p>
+        <button onClick="pop()">KNOW MORE</button>
+      </div>
+    </div>
+  </div>
+  <!-- Content 2 -->
+  <!-- About -->
+  <div class="about" id="about">
+    <div class="judul2">
+      <h1>ABOUT US</h1>
+    </div>
+    <div class="judul-kedua">
+      <p class="kedua">Know more about we are team and company</p>
+    </div>
+    <div class="isi">
+      <div class="kiri">Our company is based in Jakarta, our company has been around since 2002, we have branch companies spread throughout almost all regions of Indonesia, and our main goal is to build a better future.</div>
+      <div class="kanan">We are a team that works 15 hours per day, that's because we are professionals who want to serve our clients well and wholeheartedly, we are a fast, responsive and friendly team.</div>
+    </div>
+  </div>
+  <!-- About -->
+  <!-- Clients -->
+  <div class="project" id="client">
+    <div class="more">
+      <h1>OUR CLIENT</h1>
+      <p>Client is not just a customer, for us our clients are like our family that we care from the bottom of our hearth</p>
+    </div>
+    <div class="arsitek">
+      <div class="klien-pertama">
+        <img src="../IMG/arsitek.jpg" alt="" />
+      </div>
+      <div class="ket-arsitek">
+        <h1>ARCHITECT</h1>
+        <p>Mr. Brent Fredrickson is a professional architect, he is one of our loyal clients in the manufacture design.</p>
+        <div class="button-arsitek">
+          <button onClick="pop()">KNOW MORE</button>
+        </div>
+      </div>
+    </div>
+    <div class="pelajar">
+      <div class="klien-kedua">
+        <img src="../IMG/Winton_Almundarisna.jpg" alt="" />
+      </div>
+      <div class="ket-pelajar">
+        <h1>STUDENT</h1>
+        <p>Winton Almundarisna is an IT student at Indraprasta PGRI university, he is our IT consulting client.</p>
+        <div class="button-it">
+          <button onClick="pop()">KNOW MORE</button>
+        </div>
+      </div>
+    </div>
+    <div class="juru-masak">
+      <div class="klien-ketiga">
+        <img src="../IMG/chef.jpg" alt="" />
+      </div>
+      <div class="ket-masak">
+        <h1>CHEF</h1>
+        <p>Miss Gwen Holton is a senior chef in London who orders design and many kitchen infrastructure from us.</p>
+        <div class="button-chef">
+          <button onClick="pop()">KNOW MORE</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Clients -->
+  <!-- Message -->
+  <div class="message" id="message">
+    <div class="content-message">
+      <h1 class="judul-message">LET'S SEND YOUR MESSAGE</h1>
+      <div class="logo_message">
+        <img src="../IMG/logo_message.png" alt="" />
+      </div>
+      <form>
+        <li><label for="email">Email</label></li>
+        <li><input class="inputan" type="text" id="email" placeholder="Your Email" /></li>
+        <br />
+        <li><label for="message">Message</label></li>
+        <li><textarea class="inputan" id="message" cols="30" rows="10" placeholder="Your Message"></textarea></li>
+        <br />
+      </form>
+      <div class="send">
+        <button onClick="pop()">SEND</button>
+      </div>
+    </div>
+  </div>
+  <!-- Message -->
+  <!-- Footer -->
+  <div class="footer" id="contact">
+    <div class="location">
+      <img src="../IMG/location.png" alt="" />
+      <p>Not available now</p>
+    </div>
+    <div class="contact">
+      <img class="img_location" src="../IMG/facebook.png" alt="" />
+      <img class="img_location" src="../IMG/ig.png" alt="" />
+      <img class="img_location" src="../IMG/wa.jpg" alt="" />
+      <p>Copyright 2022, Designed By <span>Winton Almundarisna</span></p>
+    </div>
+    <div class="rate">
+      <p>
+        Can you rate this website ?
+        <button onClick="pop()">Yes i can</button>
+      </p>
+    </div>
+  </div>
+  <!-- Footer -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <!-- Transparent Navbar -->
+  <script type="text/javascript">
+    window.addEventListener("scroll", function() {
+      var header = document.querySelector("nav");
+      header.classList.toggle("sticky", window.scrollY > 0);
+    });
+  </script>
+  <script>
+    window.addEventListener("scroll", function() {
+      var dua = document.querySelector("img");
+      dua.classList.toggle("stickDua", window.scrollY > 0);
+    });
+  </script>
+  <script type="text/javascript">
+    window.addEventListener("scroll", function() {
+      var tiga = document.querySelector("span");
+      tiga.classList.toggle("stickTiga", window.scrollY > 0);
+    });
+  </script>
+  <script>
+    function pop() {
+      window.alert("Fitur ini belum tersedia karena beberapa alasan");
+    }
+  </script>
+  <!-- Transparent Navbar -->
+
+</body>
+</html>
